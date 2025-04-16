949571a11db11e7494c4ae53c0eceda51c6d2125
Aleksandra Mosyagina <a.mosyagina@list.com>
2025-04-16 11:50:39 +0300

    changed card fields

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,6 +1,6 @@
-import { FC } from 'react';
-import styles from './MovieCard.module.scss';
-import { useMovieDetails } from '../../hooks/useMovieDetails';
+import { FC } from "react";
+import styles from "./MovieCard.module.scss";
+import { useMovieDetails } from "../../hooks/useMovieDetails";
 
 export interface MovieCardProps {
   movieId?: number;
@@ -30,7 +30,7 @@ export const MovieCard: FC<MovieCardProps> = ({ movieId = null }) => {
     return (
       <div className={styles.movieCard}>
         <div>Фильм не найден</div>
-        {process.env.NODE_ENV === 'development' && (
+        {process.env.NODE_ENV === "development" && (
           <pre>
             Debug Info:
             {JSON.stringify(movie, null, 2)}
@@ -40,20 +40,19 @@ export const MovieCard: FC<MovieCardProps> = ({ movieId = null }) => {
     );
   }
 
-
   return (
     <div className={styles.movieCard}>
       {movie.poster && (
-                <img 
-                    src={`${movie.poster}`}
-                    alt={`Постер ${movie.title}`}
-                    className={styles.poster}
-                />
-            )}
+        <img
+          src={`${movie.poster}`}
+          alt={`Постер ${movie.title}`}
+          className={styles.poster}
+        />
+      )}
       <h2>{movie.title}</h2>
-      <p>Год: {movie.year}</p>
+      <p>Year: {movie.year}</p>
       <p>{movie.plot_overview}</p>
-      <p>Рейтинг: {movie.user_rating}</p>
+      <p>Movie rating: {movie.user_rating}</p>
     </div>
   );
 };
